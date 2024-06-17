02e49176b49869161903550995d977821d81a826
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-06-16 23:37:17 -0400

    Document and clean up credit's scripting

--- scenes/CreditText.cs
+++ scenes/CreditText.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class CreditText : Label
 {
+	//Used simply for styling the ending of the credits
 	int CoolEnd = 0;
 
 	// Called when the node enters the scene tree for the first time.
@@ -13,12 +14,13 @@ public partial class CreditText : Label
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		//Checks if at point in credits to cut the scrolling
 		if (CoolEnd != 2000)
 		{
+			//Scrolls and updates
 			Position = new Vector2(Position.X, Position.Y - 2);
 			CoolEnd++;
 		}
-		GD.Print(CoolEnd);
-		
+
 	}
 }
