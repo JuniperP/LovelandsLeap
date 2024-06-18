58db2c973962a54bae4a5d026199632edd05cd50
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-06-17 21:59:17 -0400

    Allow the access of pausing on every screen

--- scripts/PauseScreen.cs
+++ scripts/PauseScreen.cs
@@ -1,31 +1,32 @@
 using Godot;
 using System;
 
-public partial class pause_screen : Control
+public partial class PauseScreen : Control
 {
 	// Setup
 	public override void _Ready()
 	{
-		Visible=false;
+		Visible = false;
 	}
-	
+
 	// Close the menu
 	private void _close()
 	{
-		Visible=false;
-		GetTree().Paused=false;
+		GetTree().Paused = false;
+		Visible = false;
 	}
 
 	// Open the menu
 	private void _open()
 	{
-		Visible=true;
-		GetTree().Paused=true;
+		Visible = true;
+		GetTree().Paused = true;
 	}
 
 	// Return to the main menu
 	private void _to_main_menu()
 	{
+		_close();
 		GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
 	}
 
