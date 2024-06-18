6e6c44a28cf0f8290a615b72f622ab6d137e7d65
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-06-17 21:20:17 -0400

    Pause screen pauses all actions occurring in the background

--- scripts/pause_screen.cs
+++ scripts/pause_screen.cs
@@ -13,18 +13,30 @@ public partial class pause_screen : Control
 	private void _close()
 	{
 		Visible=false;
+		GetTree().Paused=false;
 	}
 
 	// Open the menu
 	private void _open()
 	{
 		Visible=true;
+		GetTree().Paused=true;
 	}
 
 	// Return to the main menu
 	private void _to_main_menu()
 	{
 		GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		// Sees if the user is trying to pause the game
+		if (Input.IsActionPressed("ui_cancel"))
+		{
+			_open();
+		}
 	}
 }
 
