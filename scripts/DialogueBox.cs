9108c271a22a45e5ab13c64a2dfcf0499beccc36
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-06-19 19:31:43 -0400

    Update dialogue text node name

--- scripts/DialogueBox.cs
+++ scripts/DialogueBox.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class DialogueBox : Toggleable
 {
-    // Called when the node enters the scene tree for the first time.
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Visible=false;
