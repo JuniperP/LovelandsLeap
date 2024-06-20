41a13e0b12817cc1248a417ef3545bca07f29368
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-06-20 17:53:37 -0400

    Implement grapple cancel on collision

--- scripts/player/GrappleState.cs
+++ scripts/player/GrappleState.cs
@@ -7,16 +7,24 @@ public class GrappleState : IMovementState
         // Set velocity to move to weight
         Vector2 diff = ctx.TongueWeight.GlobalPosition - ctx.GlobalPosition;
         ctx.Velocity = diff * 10;
-
+        
+        if (ctx.MoveAndSlide()) // If player floored
+            DisableGrapple(ctx);
     }
 
-    public void HandleAction(Player ctx)
-    {
+	public void HandleAction(Player ctx) { }
 
-    }
+	public void EnableGrapple(Player ctx) { }
 
-    public void EnableGrapple(Player ctx)
-    {
+	public void DisableGrapple(Player ctx)
+	{
+		// Remove all grappling objects
+		ctx.TongueLine.QueueFree();
+		ctx.TongueSpring.QueueFree();
+		ctx.TongueWeight.QueueFree();
 
-    }
+		// Change state to walk
+		ctx.MovementState = new WalkState();
+		ctx.StateEnum = Player.State.Walk;
+	}
 }
