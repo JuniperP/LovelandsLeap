428f67364ce3cdda32942f6f15760e3645700a72
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-06-20 21:27:34 -0400

    Remove unnecessary float cast

--- scripts/player/TongueSpring.cs
+++ scripts/player/TongueSpring.cs
@@ -11,7 +11,7 @@ public partial class TongueSpring : DampedSpringJoint2D
 		NodeB = Target.GetPath(); // Set secondary spring node to target
 
 		LookAt(Target.GlobalPosition);
-		Rotate((float)-0.5f * Mathf.Pi); // NECESSARY, but no clue why :3
+		Rotate(-0.5f * Mathf.Pi); // NECESSARY, but no clue why :3
 		Length = LengthFactor * GlobalPosition.DistanceTo(Target.GlobalPosition);
 	}
 }
