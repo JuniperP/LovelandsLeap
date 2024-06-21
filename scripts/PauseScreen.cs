f183155fe62f4fd62468a77d4452eaf56ae35774
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-06-20 22:51:52 -0400

    Escape to leave the pause screen

--- scripts/PauseScreen.cs
+++ scripts/PauseScreen.cs
@@ -4,10 +4,14 @@ using System.Runtime.CompilerServices;
 
 public partial class PauseScreen : Toggleable
 {
+	// Effectively the timer we use to ensure a reasonable amount of time passes between our events
+	double counter;
+
 	// Setup
 	public override void _Ready()
 	{
 		Visible = false;
+		counter = 0;
 	}
 
 	// Close the menu
@@ -34,11 +38,32 @@ public partial class PauseScreen : Toggleable
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// Sees if the user is trying to pause the game
-		if (Input.IsActionPressed("ui_cancel"))
+		//Counting up time since the last escape press 
+		if (counter < .5)
 		{
-			_open();
+			counter += delta;
 		}
+
+
+		// Sees if the user is trying to pause the game
+		if (Input.IsActionPressed("ui_cancel") && (counter>=.5))
+		{
+			// Reset the timer
+			counter = 0;
+
+			//Switch visibility
+			if (Visible)
+			{
+				_close();
+			}
+			else
+			{
+				_open();
+			}
+
+		}
+
+
 	}
 }
 
