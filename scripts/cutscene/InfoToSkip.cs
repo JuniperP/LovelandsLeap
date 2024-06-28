7f7028f5dcfc022871df80f25c83b72c278d3377
Juniper <juniper.pasternak@gmail.com>
2024-06-28 13:22:38 -0400

    Use formatted string in InfoToSkip

--- scripts/cutscene/InfoToSkip.cs
+++ scripts/cutscene/InfoToSkip.cs
@@ -3,10 +3,8 @@ using System;
 
 public partial class InfoToSkip : Label
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Text = "Press ["+Keybinds.CancelSym+"] to skip";
+		Text = $"Press [{Keybinds.CancelSym}] to skip";
 	}
-
 }
