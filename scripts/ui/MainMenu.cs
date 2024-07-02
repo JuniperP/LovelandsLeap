3ac7ee584b034a441e3f3725788eeb9a54dd6630
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-02 00:39:00 -0400

    Remove useless ready in main

--- scripts/ui/MainMenu.cs
+++ scripts/ui/MainMenu.cs
@@ -3,14 +3,6 @@ using System;
 
 public partial class MainMenu : Control
 {
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		// Loads in save data
-		// SaveGame.LoadData();
-	}
-
 	// Starts the game
 	private void _on_start_game_button_pressed()
 	{
