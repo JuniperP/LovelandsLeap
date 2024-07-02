d24d5b814f3c00f5e315a6e59582a617436b946c
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-02 15:37:35 -0400

    Implement DFS for assigning buttons sfx

--- scripts/player/SoundManager.cs
+++ scripts/player/SoundManager.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 
 
 // Enums for all of the possible name
-public enum SFX :  byte
+public enum SFX : byte
 {
 	Walk,
 	Jump,
 	Land,
 	TongueShoot,
 	TongueHit,
-	Croak, 
+	Croak,
 	DialogueMC,
 	DialogueFrog,
 	DialogueWitch,
@@ -48,32 +48,32 @@ public partial class SoundManager : Node
 		NewPlayer.Bus = "Sound Effects";
 
 		// Giant if statement mess cause I couldn't think of a better way to assign names and files
-		if(sfx==SFX.Walk)
+		if (sfx == SFX.Walk)
 			NewPlayer.Name = "temporary_sfx";
-		else if(sfx==SFX.Jump)
+		else if (sfx == SFX.Jump)
 			NewPlayer.Name = "jump_sfx";
-		else if(sfx==SFX.Land)
+		else if (sfx == SFX.Land)
 			NewPlayer.Name = "temporary_sfx";
-		else if(sfx==SFX.TongueShoot)
+		else if (sfx == SFX.TongueShoot)
 			NewPlayer.Name = "tongue_shoot_sfx";
-		else if(sfx==SFX.TongueHit)
+		else if (sfx == SFX.TongueHit)
 			NewPlayer.Name = "tongue_hit_sfx";
-		else if(sfx==SFX.Croak)
+		else if (sfx == SFX.Croak)
 			NewPlayer.Name = "temporary_sfx";
-		else if(sfx==SFX.DialogueMC)
+		else if (sfx == SFX.DialogueMC)
 			NewPlayer.Name = "dialogue_sfx";
-		else if(sfx==SFX.DialogueFrog)
+		else if (sfx == SFX.DialogueFrog)
 			NewPlayer.Name = "dialogue_sfx";
-		else if(sfx==SFX.DialogueWitch)
+		else if (sfx == SFX.DialogueWitch)
 			NewPlayer.Name = "dialogue_sfx";
-		else if(sfx==SFX.DialoguePrincess)
+		else if (sfx == SFX.DialoguePrincess)
 			NewPlayer.Name = "dialogue_sfx";
-		else if(sfx==SFX.UIButton)
+		else if (sfx == SFX.UIButton)
 			NewPlayer.Name = "ui_buttons_sfx";
-			
-		
+
+
 		// Assigning the file for each sound
-		NewPlayer.Stream = (AudioStream) GD.Load($"res://audio/sfx/{NewPlayer.Name}.wav");
+		NewPlayer.Stream = (AudioStream)GD.Load($"res://audio/sfx/{NewPlayer.Name}.wav");
 
 		// Gives back our new player
 		return NewPlayer;
@@ -84,25 +84,42 @@ public partial class SoundManager : Node
 	public static void PlaySound(SFX Sound, Node PlayOn)
 	{
 		// Adds the sound to the node tree if not already there
-		if(PlayOn.FindChild(_sounds[Sound].Name, false, false) == null)
+		if (PlayOn.FindChild(_sounds[Sound].Name, false, false) == null)
 		{
 			PlayOn.AddChild(_sounds[Sound]);
 		}
 
 		// Plays the sound effect
 		_sounds[Sound].Play();
+
+
 	}
 
-
-
 	// Recursive post-order depth 1st search through children assigning sound effects to UI buttons
-	private static void ApplyButtonSFX(Node node)
+	public static void ApplyButtonSFX(Node CurrentNode, bool first)
 	{
-		//Goes through each child
-			//Calls this function again but with each child
-			//Checks if child is a button
-				//If is apply signal to do lambda that does: SoundManager.PlaySound(SoundManager.SFX.UIButton);
+		// Ensuring all children have access to the sound to play
+		if(first)
+		{
+			CurrentNode.AddChild(_sounds[SFX.UIButton]);
+			first = false;
+		}
 
+		// Getting all of the children
+		Godot.Collections.Array<Node> Children = CurrentNode.GetChildren();
+
+		for (int i = 0; i < Children.Count; i++)
+		{
+			
+			// Recursively going down as far as possible into the tree
+			ApplyButtonSFX(Children[i], first);
+
+			// If a button is found it is set up to make a sound on click
+			if (Children[i].GetClass() == "Button")
+				((Button)CurrentNode.GetChildren()[i]).Pressed += () => _sounds[SFX.UIButton].Play();
+
+		}
+		
 	}
 
 	/*
