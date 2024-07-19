396f63e5867838db5f3cca0add98a3ac98a28caf
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-19 16:50:35 -0400

    Create method for loading game data

--- scripts/ui/loading/LoadLevelData.cs
+++ scripts/ui/loading/LoadLevelData.cs
@@ -6,16 +6,21 @@ public partial class LoadLevelData : Node
 	// Where we will be saving all non config data to
 	private static string _saveTo = "user://lovelandsavedata.json";
 
-	// Dictionary of what stored values correspond to what
-	private static readonly Dictionary<ToScene, int> _level = new()
+	// Dictionaries of what stored values correspond to what
+	private static readonly Dictionary<ToScene, int> _levelToInt = new()
 	{
 		{ToScene.PlayTestLevel, 0},
+	};
+	private static readonly Dictionary<int, ToScene> _intToLevel = new()
+	{
+		{0, ToScene.PlayTestLevel},
 	};
 
 
 	/*
 		Method to store game data.
-		Currently just stores level data, but feel free to add on more.
+		Note 1: Currently just stores level data, but feel free to add on more.
+		Note 2: Storing via a dictionary entry to make debugging easier.
 	*/
 	public static void SaveData(ToScene level)
 	{
@@ -23,9 +28,23 @@ public partial class LoadLevelData : Node
 		FileAccess saveFile = FileAccess.Open(_saveTo, FileAccess.ModeFlags.Write);
 
 		// Converting where the player currently is into json format
-		string jsonString = Json.Stringify((Godot.Collections.Dictionary) new() { { "PlayerAtLevel", _level[level] } });
+		string jsonString = Json.Stringify((Godot.Collections.Dictionary)new() { { "PlayerAtLevel", _levelToInt[level] } });
 
 		// Storing data
 		saveFile.StoreLine(jsonString);
+	}
+
+
+	public static ToScene LoadData()
+	{
+		// Getting where we saved from
+		FileAccess saveFile = FileAccess.Open(_saveTo, FileAccess.ModeFlags.Write);
+
+		// Getting the value of the level from the save file
+		Godot.Collections.Dictionary level = (Godot.Collections.Dictionary)Json.ParseString(saveFile.GetLine());
+
+		// Converting from dictionary to int
+		return _intToLevel[(int)level["PlayerAtLevel"]];
+
 	}
 }
