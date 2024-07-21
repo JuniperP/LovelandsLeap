7318aee574f95dff9a2fce7ab6e35f010eac843c
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-21 17:01:41 -0400

    Make timer toggleable

--- scripts/ui/SpeedRunTimer.cs
+++ scripts/ui/SpeedRunTimer.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class SpeedRunTimer : Panel
+public partial class SpeedRunTimer : Toggleable
 {
 	// Total time elapsed
 	private static float _timeElapsed = 0;
