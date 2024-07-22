47cd19bef89beb8780701689795d38237fbc8625
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-22 14:58:11 -0400

    Refactor ConfirmReset

--- scripts/ui/settings/ConfirmReset.cs
+++ scripts/ui/settings/ConfirmReset.cs
@@ -1,14 +1,9 @@
 public partial class ConfirmReset : Toggleable
 {
-
-	// Boolean signifying if a reset occurred
-	public static bool SettingsReset;
-
 	// Used to reset the default settings
 	private void ResetSettings()
 	{
 		LoadSettingsData.LoadData(true);
-		SettingsReset = true;
 		Close();
 	}
 }
