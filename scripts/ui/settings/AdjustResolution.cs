b3da5d7b9a4ebb5b34d91ec60f226a54185d920c
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-22 15:45:00 -0400

    Create outline for resolution changer class

---
+++ scripts/ui/settings/AdjustResolution.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class AdjustResolution : OptionButton
+{
+	// The users selected Resolution option
+	public static int userID = -1;
+
+
+	// Done at start of game and when display changes
+	public static void PopulateOptions()
+	{
+		// Destroy current resolution options
+
+		// Populate drop down with new resolutions
+		
+		// Select default option
+	}
+
+
+	// Signaled when new drop down option is selected
+	public static void NewResolutionSet(int id)
+	{
+		userID = id;
+		
+		// AdjustScreenResolution(get(userID));
+
+	}
+
+	// Helper func to set resolution
+	public static void AdjustScreenResolution(/*Resolution res*/)
+	{
+		// New resolution = res
+	}
+
+}
+
