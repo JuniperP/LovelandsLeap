139c532ce563278f52c729bd3fc5c7873122fcbc
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-24 16:36:53 -0400

    Allow instances of monitor stand ins to have different numbers via export

---
+++ scripts/ui/settings/monitor_stand_in.cs
@@ -0,0 +1,13 @@
+using Godot;
+public partial class monitor_stand_in : Control
+{
+	// The number displayed on the screen of the monitor symbol
+	[Export] public int number;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Setting the number of the monitor
+		GetNode<TextureRect>("MonitorLogo").GetNode<Label>("GivenNumber").Text = $"{number}";
+	}
+}
