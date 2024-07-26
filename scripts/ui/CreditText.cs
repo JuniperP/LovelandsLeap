6780f6b307720cacd54cff4c27ce93462eb849c8
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-07-26 17:29:36 -0400

    Make credits framerate and text-size independent

--- scripts/ui/CreditText.cs
+++ scripts/ui/CreditText.cs
@@ -2,16 +2,14 @@ using Godot;
 
 public partial class CreditText : Label
 {
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		// Checks if at point in credits to cut the scrolling
-		if (Position.Y > -6400)
+		if (Position.Y > -(Size.Y - 1000))
 		{
 			// Scrolls and updates
-			Position = new Vector2(Position.X, Position.Y - 2);
+			Position = new Vector2(Position.X, Position.Y - (120 * (float)delta));
 		}
-
 	}
 }
--- scripts/ui/Credits.cs
+++ scripts/ui/Credits.cs
@@ -13,7 +13,6 @@ public partial class Credits : Control
 		// SpeedRunTimer.FinishedRun();
 	}
 
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
