ffa1cb2c1b5766a7af9059169c20d54d7566821b
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-28 13:53:59 -0400

    Document camera and extensions

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -2,8 +2,10 @@ using Godot;
 
 public partial class Camera : Camera2D
 {
+	// Target that the camera follows
 	[Export] public Node2D Target;
 
+	// Limits are the furthest pixels the camera will see
 	[ExportGroup("CustomLimits")]
 	[Export] public int Left = -10000000;
 	[Export] public int Top = -10000000;
@@ -14,8 +16,10 @@ public partial class Camera : Camera2D
 	{
 		Vector2 targetPos = Target.GlobalPosition;
 
-		// Not technically radii but half the length and width
+		// Half the (length, height) vector of the screen
 		Vector2 camRadii = GetViewportRect().Size / 2;
+
+		// The top left and bottom right positions allowed for the camera
 		Vector2 minPos = new(Left + camRadii.X, Top + camRadii.Y);
 		Vector2 maxPos = new(Right - camRadii.X, Bottom - camRadii.Y);
 
--- scripts/Extensions.cs
+++ scripts/Extensions.cs
@@ -1,7 +1,9 @@
 using Godot;
 
+// Extensions allow additional methods for existing classes
 public static class Extensions
 {
+	// Checks 3 conditions of a GodotObject ensuring validity
 	public static bool IsValid<T>(this T node) where T : GodotObject
 	{
 		return node is not null
