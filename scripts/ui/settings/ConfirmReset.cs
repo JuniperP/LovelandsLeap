f95d8859db11d1e0d63b02dfcf88267fdbe873db
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-07-29 16:29:50 -0400

    Make factory resets send your screen to the preferred monitor

--- scripts/ui/settings/ConfirmReset.cs
+++ scripts/ui/settings/ConfirmReset.cs
@@ -1,9 +1,20 @@
+using Godot;
+
 public partial class ConfirmReset : Toggleable
 {
 	// Used to reset the default settings
 	private void ResetSettings()
 	{
+		// Loading in the new data
 		LoadSettingsData.LoadData(true);
+
+		// Adjusting screen accordingly
+		if (MonitorStandIn.ChosenScreen >= DisplayServer.GetScreenCount())
+			MonitorStandIn.TempScreenChoice = GetWindow().CurrentScreen;
+		else
+			DisplayServer.WindowSetCurrentScreen(MonitorStandIn.ChosenScreen, GetWindow().GetWindowId());
+
+		// Closing the screen
 		Close();
 	}
 }
