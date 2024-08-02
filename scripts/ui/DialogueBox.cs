824c782006fc564f4906e28b5cc23d8eaae7eb92
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-08-02 19:34:27 -0400

    Outline DialogueBox

--- scripts/ui/DialogueBox.cs
+++ scripts/ui/DialogueBox.cs
@@ -1,7 +1,41 @@
+using Godot;
+
 public partial class DialogueBox : Toggleable
 {
-	/*
-		Currently nothing further in here but this will likely
-		change once different types of dialogue are used.
-	*/
+	private enum State
+	{
+		Inactive,
+		Loading,
+		Paused
+	}
+
+	private State _loadState = State.Inactive;
+
+	private void Step()
+	{
+		if (_loadState == State.Inactive)
+			Load();
+		else if (_loadState == State.Loading)
+			Complete();
+		else if (_loadState == State.Paused)
+			Unload();
+	}
+
+	private void Load()
+	{
+		// Fade in box and start loading in dialogue
+		_loadState = State.Loading;
+	}
+
+	private void Complete()
+	{
+		// Complete fade and text
+		_loadState = State.Paused;
+	}
+
+	private void Unload()
+	{
+		// Fade out and perform callback
+		_loadState = State.Inactive;
+	}
 }
