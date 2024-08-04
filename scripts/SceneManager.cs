c4a932f78f90aa6cadb7398927bafb06eaaa7697
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-04 15:29:23 -0400

    Set up for future levels

--- scripts/SceneManager.cs
+++ scripts/SceneManager.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 // Enums for all of the possible scenes transitioned to
 public enum ToScene : int
 {
-	MainMenu = 1,
-	Credits = 2,
-	IntroCutscene = 3,
-	Monitor = 4,
-	PlayTestLevel = 5,
-	Level1 = 6,
+	MainMenu = -1,
+	Credits = -2,
+	IntroCutscene = -3,
+	Monitor = -4,
+	PlayTestLevel = 0,
+	Tutorial = 1,
+	Level1 = 2,
+	Level2 = 3,
+	Level3 = 4,
+	Level4 = 5,
+
 }
 public partial class SceneManager : Node
 {
@@ -33,7 +38,11 @@ public partial class SceneManager : Node
 		{ToScene.IntroCutscene,  "res://scenes/cutscenes/intro.tscn"},
 		{ToScene.Monitor,  "res://scenes/ui/settings/monitor_stand_in.tscn"},
 		{ToScene.PlayTestLevel,  "res://scenes/levels/play_test.tscn"},
+		{ToScene.Tutorial,  "res://scenes/levels/tutorial.tscn"},
 		{ToScene.Level1,  "res://scenes/levels/level_1.tscn"},
+		{ToScene.Level2,  "res://scenes/levels/level_2.tscn"},
+		{ToScene.Level3,  "res://scenes/levels/level_3.tscn"},
+		{ToScene.Level4,  "res://scenes/levels/level_4.tscn"},
 	};
 
 	// Allows the client to set where to go to next
