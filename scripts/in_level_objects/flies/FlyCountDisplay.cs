cedf9d15f79b520ed8262eb52f6e02e926a118cc
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-04 16:51:07 -0400

    Have fly count appear only when not giving input

--- scripts/in_level_objects/flies/FlyCountDisplay.cs
+++ scripts/in_level_objects/flies/FlyCountDisplay.cs
@@ -1,16 +1,51 @@
 using Godot;
-using System;
 
 public partial class FlyCountDisplay : Panel
 {
+	// How we will count up to 3
+	private double count;
+
+	// Checking if we 
+	[Export] private bool _isInLevel;
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		count = 0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_isInLevel)
+		{
+			if (GetTree().Paused)
+				Hide();
+
+			else
+			{
+				if (!Visible && count >= 2)
+					Show();
+
+				else if (Visible && count < 2)
+					Hide();
+
+				if (Input.IsAnythingPressed() == false)
+					count += delta;
+
+				else
+					count = 0;
+			}
+
+		}
+
+		else
+		{
+			if (!Visible)
+				Show();
+		}
+
 	}
 
 	// Updating the results every time for every time visibility changes
