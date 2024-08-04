6d5d93d111b2139acbc14da9a7e5d54cd5c62a30
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-04 15:13:34 -0400

    Adjust total fly label to change off visibility

--- scripts/in_level_objects/flies/CurrentCountLabel.cs
+++ scripts/in_level_objects/flies/CurrentCountLabel.cs
@@ -2,21 +2,9 @@ using Godot;
 
 public partial class CurrentCountLabel : Label
 {
-	// The second half of the label saying making the count fraction
-	private string _outOf;
-
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	// Updating the results every time for every time visibility changes
+	public void UpdateText()
 	{
-		_outOf = $"/{FlyCount.TotalFlies}";
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		// Making the label show the current fraction of flies gotten
-		if (Visible)
-			Text = FlyCount.FliesGotten + _outOf;
+		Text = $"Total: {FlyCount.FliesGotten}/{FlyCount.TotalFlies}";
 	}
 }
