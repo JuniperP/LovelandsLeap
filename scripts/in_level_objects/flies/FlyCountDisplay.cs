e8d09d0fc64fa5437aac4684c5b879bf77201447
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-06 16:00:39 -0400

    Make fly counter static in the top left corner

--- scripts/in_level_objects/flies/FlyCountDisplay.cs
+++ scripts/in_level_objects/flies/FlyCountDisplay.cs
@@ -2,52 +2,8 @@ using Godot;
 
 public partial class FlyCountDisplay : Panel
 {
-	// How we will count up to 3
-	private double _count;
-
-	private float _currentFade;
-
-	// Checking if we 
-	[Export] private bool _isInLevel;
-
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_count = 0;
-		_currentFade = 0;
-	}
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
-		if (_isInLevel)
-		{
-			if (GetTree().Paused)
-				Modulate = new Color(1, 1, 1, 0);
-
-			else
-			{
-				if (_count >= 2)
-					_currentFade += (float)delta;
-
-				if (Input.IsAnythingPressed() == false)
-					_count += delta;
-
-				else
-				{
-					_count = 0;
-					_currentFade = 0;
-				}
-
-				Modulate = new Color(1, 1, 1, _currentFade);
-			}
-
-		}
-	}
-
-	// Updating the results every time for every time visibility changes
-	public void UpdateText()
 	{
 		// Updating the current levels text
 		Label level = GetNode<Label>("CurrentCountLevel");
@@ -58,4 +14,5 @@ public partial class FlyCountDisplay : Panel
 		Label total = GetNode<Label>("CurrentCountInTotal");
 		total.Text = $"Total: {FlyCount.FliesGottenTotal}/{FlyCount.TotalFlies}";
 	}
+
 }
