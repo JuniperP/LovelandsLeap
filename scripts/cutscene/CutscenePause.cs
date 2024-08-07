a42ebe502ab77d1b400af0a8396ceb2b53ccfa89
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-08-07 17:07:48 -0400

    Add delay to beginning of intro cutscene

---
+++ scripts/cutscene/CutscenePause.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+
+public partial class CutscenePause : Timer, ICutsceneElement
+{
+    public override void _Ready()
+    {
+        OneShot = true;
+    }
+
+    public void Step(Action callback)
+    {
+        // Start the timer and perform callback upon timeout
+        if (TimeLeft <= 0d)
+        {
+            Timeout += callback;
+            Start();
+        }
+        // Stop the timer, automatically performing callback
+        else
+            Stop();
+    }
+}
