83de7d267796a8b01e84f7b16a9ac0616540d2ff
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-12 14:36:17 -0400

    Set up to track objects position on track system

--- scripts/in_level_objects/tracks/LeaderTrack.cs
+++ scripts/in_level_objects/tracks/LeaderTrack.cs
@@ -15,11 +15,25 @@ public partial class LeaderTrack : Area2D
 	// What will rid the railway
 	[Export] public PackedScene SceneOnTrack;
 
+	// Given scenes position on the track
+	private Vector2 _position;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// Using our hitbox to get the line our object will follow
 		_lineToFollow = (SegmentShape2D)HitBox.Shape;
+
+		// Starting the user at the beginning of the track
+		_position = _lineToFollow.A;
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		
 	}
 
 
