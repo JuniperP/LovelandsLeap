57e1a04475ae3fa59752b0cf25143f2eb91caee9
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-12 20:50:13 -0400

    Refactor track system into using class hierarchy

--- scripts/in_level_objects/tracks/LeaderTrack.cs
+++ scripts/in_level_objects/tracks/LeaderTrack.cs
@@ -3,27 +3,15 @@ using Godot;
 /*
 First track in a rail way system.
 This track will outline the properties by which follower tracks will obey.
+
+Note: If you want a single line back and forth, use this track as it can
+bounce off endings of the track.
 */
-public partial class LeaderTrack : Area2D
+public partial class LeaderTrack : Track
 {
-
-	// The track for our object
-	private SegmentShape2D _lineToFollow;
-
-	// What will ride the railway
-	private Node2D _instanScene;
-
 	// Helper bool for tracks that require bouncing
 	private bool _towardA;
 
-	// Helper bool to say if the item on the track should be moving
-	private bool _move;
-
-	[Signal] public delegate void ReachedEndEventHandler(Node2D node, int trackSpeed);
-
-
-	[ExportGroup("SetUp")]
-	[Export] public CollisionShape2D HitBox;
 	[Export] public PackedScene SceneOnTrack;
 
 	[ExportGroup("PropertiesOfTheTrack")]
@@ -33,65 +21,50 @@ public partial class LeaderTrack : Area2D
 
 
 
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	protected override void FurtherSetup()
 	{
-		// Using our hitbox to get the line our object will follow
-		_lineToFollow = (SegmentShape2D)HitBox.Shape;
-
-		_instanScene = SceneOnTrack.Instantiate<Node2D>();
-		AddChild(_instanScene);
+		// The the start of the system thus creates the scene to use
+		InstanScene = SceneOnTrack.Instantiate<Node2D>();
+		AddChild(InstanScene);
 
 		// Starting the user at the beginning of the track
-		_instanScene.Position = _lineToFollow.A;
+		InstanScene.Position = LineToFollow.A;
 
 		_towardA = false;
-		_move = AutoStart;
-
-	}
-
-	private void ToggleTrack()
-	{
-		_move = !_move;
+		Move = AutoStart;
+		SpeedMod = TrackSpeed;
 	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (_instanScene.IsValid() && _move)
+		if (InstanScene.IsValid() && Move)
 		{
-			float nodeSpeed = TrackSpeed * 100 * (float)delta;
+			float nodeSpeed = SpeedMod * 100 * (float)delta;
 
 			if (Bounce)
-			{
-				// Going the right direction
-				if (_towardA)
-					_instanScene.Position = _instanScene.Position.MoveToward(_lineToFollow.A, nodeSpeed);
-				else
-					_instanScene.Position = _instanScene.Position.MoveToward(_lineToFollow.B, nodeSpeed);
-
-				// Flipping direction once a side has been hit
-				if (_instanScene.Position == _lineToFollow.A)
-					_towardA = false;
-				else if (_instanScene.Position == _lineToFollow.B)
-					_towardA = true;
-			}
+				MoveBounce(nodeSpeed);
 
 			else
-			{
-				_instanScene.Position = _instanScene.Position.MoveToward(_lineToFollow.B, nodeSpeed);
-
-				// Passing the scene if the end is reached
-				if (_instanScene.Position == _lineToFollow.B)
-				{
-					EmitSignal(SignalName.ReachedEnd, _instanScene, TrackSpeed);
-					_move = false;
-				}
-					
-			}
+				MoveNoBounce(nodeSpeed);
 		}
+	}
+
+	// Used to move if their is bounce
+	private void MoveBounce(float nodeSpeed)
+	{
+		// Going the right direction
+		if (_towardA)
+			InstanScene.Position = InstanScene.Position.MoveToward(LineToFollow.A, nodeSpeed);
+		else
+			InstanScene.Position = InstanScene.Position.MoveToward(LineToFollow.B, nodeSpeed);
+
+		// Flipping direction once a side has been hit
+		if (InstanScene.Position == LineToFollow.A)
+			_towardA = false;
+		else if (InstanScene.Position == LineToFollow.B)
+			_towardA = true;
 	}
 
 
---
+++ scripts/in_level_objects/tracks/Track.cs
@@ -0,0 +1,78 @@
+using Godot;
+
+public partial class Track : Node
+{
+
+	//Orient option
+	//Sub class make
+	//loopable
+	//stop all aspects of the track
+
+
+	// The track for our object
+	protected SegmentShape2D LineToFollow;
+
+	// What will ride the railway
+	protected Node2D InstanScene;
+
+	// How fast the track shall go
+	protected float SpeedMod;
+
+	// Helper bool to say if the item on the track should be moving
+	protected bool Move;
+
+	[Signal] public delegate void ReachedEndEventHandler(Node2D node, int trackSpeed);
+
+	[Export] public CollisionShape2D HitBox;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Using our hitbox to get the line our object will follow
+		LineToFollow = (SegmentShape2D)HitBox.Shape;
+
+		FurtherSetup();
+	}
+
+	// Starts up the track once it is passed the node by signals
+	protected void StartTrack(Node2D node, int trackSpeed)
+	{
+		SpeedMod = trackSpeed;
+		node.Position = LineToFollow.A;
+		InstanScene = node;
+		Move = true;
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (InstanScene.IsValid() && Move)
+			MoveNoBounce(SpeedMod * 100 * (float)delta);
+	}
+
+
+	// Used to move if their is no bounce 
+	protected void MoveNoBounce(float nodeSpeed)
+	{
+		InstanScene.Position = InstanScene.Position.MoveToward(LineToFollow.B, nodeSpeed);
+
+		// Passing the scene if the end is reached
+		if (InstanScene.Position == LineToFollow.B)
+		{
+			EmitSignal(SignalName.ReachedEnd, InstanScene, SpeedMod);
+			Move = false;
+		}
+	}
+
+	
+	// To be used by special types of tracks that require more setup
+	protected virtual void FurtherSetup()
+	{
+		InstanScene = null;
+		Move = false;
+		SpeedMod = 0;
+	}
+
+}
