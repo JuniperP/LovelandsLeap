fcfe0f7f197a2c4989f4890bae58c08ee1539181
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-21 16:28:00 -0400

    Have play test button start plat theme

--- scripts/ui/settings/key_binds/PlayTestButton.cs
+++ scripts/ui/settings/key_binds/PlayTestButton.cs
@@ -4,6 +4,7 @@ public partial class PlayTestButton : Button
 	// Telling the node that its time to play test
 	private static void PlayTest()
 	{
+		LoadingScreen.NeedsToStartPlatTheme = true;
 		SceneManager.SetNextGoTo(ToScene.PlayTestLevel);
 		LoadingScreen.FadeIn();
 	}
