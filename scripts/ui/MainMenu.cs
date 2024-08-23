665ef17a25d925d262c3b118d332a520030d5613
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-08-23 16:59:13 -0400

    Set up all intro scenes to send you to the tutorial

--- scripts/ui/MainMenu.cs
+++ scripts/ui/MainMenu.cs
@@ -24,7 +24,7 @@ public partial class MainMenu : Control
 	private void StartNewGame()
 	{
 		// Overwrite save data
-		LoadLevelData.SaveData(ToScene.Level1);
+		LoadLevelData.SaveData(ToScene.Tutorial);
 
 		// Accounting for speedrun
 		if (ToggleSpeedrun.HasTimer)
