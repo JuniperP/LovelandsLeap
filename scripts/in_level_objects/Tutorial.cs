7e89988727d8190b71c207a577ab50ff92a4f7e8
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-04 16:45:10 -0400

    Method decompose the update text functionality of tutorial

--- scripts/in_level_objects/Tutorial.cs
+++ scripts/in_level_objects/Tutorial.cs
@@ -19,15 +19,8 @@ public partial class Tutorial : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Assigning all of the text using their keybinds
-		_walk.Text = $"Walk using [{Keybinds._acts[UserAction.Left].Input.AsText()}] \nand [{Keybinds._acts[UserAction.Right].Input.AsText()}]";
-		_jump.Text = $"Jump using [{Keybinds._acts[UserAction.Jump].Input.AsText()}]";
-		_holdJump.Text = $"Hold [{Keybinds._acts[UserAction.Jump].Input.AsText()}] to\njump higher";
-		_useTongue.Text = $"Point your mouse and\npress [{Keybinds._acts[UserAction.PAction].Input.AsText()}]\nto use your tongue\n and hit the button";
-		_jumpAndHold1.Text = $"Jump and hold onto the\nplatform with [{Keybinds._acts[UserAction.Jump].Input.AsText()}] &\n[{Keybinds._acts[UserAction.PAction].Input.AsText()}]";
-		_jumpAndHold2.Text = $"Disengage with\n[{Keybinds._acts[UserAction.SAction].Input.AsText()}]";
-		_swing.Text = $"Press [{Keybinds._acts[UserAction.Left].Input.AsText()}] and [{Keybinds._acts[UserAction.Right].Input.AsText()}]\nto swing yourself\nwhile grappled";
-		_pause.Text = $"Press [{Keybinds._acts[UserAction.Cancel].Input.AsText()}]\nat any time to\npause the game";
+		// Setting up the explanation texts
+		UpdateText();
 
 		foreach (Node node in GetChildren())
 		{
@@ -46,5 +39,18 @@ public partial class Tutorial : Node
 	private void DisengageTongue()
 	{
 		_useTongue.Text = $"Disengage your tongue by\npressing [{Keybinds._acts[UserAction.SAction].Input.AsText()}]\nor touching a surface";
+	}
+
+	private void UpdateText()
+	{
+		// Assigning all of the text using their keybinds
+		_walk.Text = $"Walk using [{Keybinds._acts[UserAction.Left].Input.AsText()}] \nand [{Keybinds._acts[UserAction.Right].Input.AsText()}]";
+		_jump.Text = $"Jump using [{Keybinds._acts[UserAction.Jump].Input.AsText()}]";
+		_holdJump.Text = $"Hold [{Keybinds._acts[UserAction.Jump].Input.AsText()}] to\njump higher";
+		_useTongue.Text = $"Point your mouse and\npress [{Keybinds._acts[UserAction.PAction].Input.AsText()}]\nto use your tongue\n and hit the button";
+		_jumpAndHold1.Text = $"Jump and hold onto the\nplatform with [{Keybinds._acts[UserAction.Jump].Input.AsText()}] &\n[{Keybinds._acts[UserAction.PAction].Input.AsText()}]";
+		_jumpAndHold2.Text = $"Disengage with\n[{Keybinds._acts[UserAction.SAction].Input.AsText()}]";
+		_swing.Text = $"Press [{Keybinds._acts[UserAction.Left].Input.AsText()}] and [{Keybinds._acts[UserAction.Right].Input.AsText()}]\nto swing yourself\nwhile grappled";
+		_pause.Text = $"Press [{Keybinds._acts[UserAction.Cancel].Input.AsText()}]\nat any time to\npause the game";
 	}
 }
