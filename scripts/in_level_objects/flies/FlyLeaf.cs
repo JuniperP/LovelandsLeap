58d5d60124321fd874b33299982fef57f25fa25f
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-03 23:08:25 -0400

    Replace the fly count with a new leaf design

---
+++ scripts/in_level_objects/flies/FlyLeaf.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+public partial class FlyLeaf : Control
+{
+	[Export] public Label InLevel;
+	[Export] public Label Total; 
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		// Updating the current levels text
+		InLevel.Text = $"{FlyCount.FliesGottenLevel} / {FlyCount.TotalLevelFlies}";
+
+		// Updating the total numbers text
+		Total.Text = $"{FlyCount.FliesGottenTotal} / {FlyCount.TotalGameFlies}";
+	}
+}
