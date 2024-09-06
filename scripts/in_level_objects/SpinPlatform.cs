b92654d007f734f522fa8ef4d287fc736a112104
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-06 03:37:46 -0400

    Further development on level 7

--- scripts/in_level_objects/SpinPlatform.cs
+++ scripts/in_level_objects/SpinPlatform.cs
@@ -2,9 +2,16 @@ using Godot;
 
 public partial class SpinPlatform : AnimatableBody2D
 {
+	[Export] public float Speed = -1;
+	[Export] public bool test = false;
+
+	// Don't ask me why this is needed 
+	private float _middleMan=0f;
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.Rotate((float)-delta);
+		_middleMan += (float)delta * Speed;
+		Rotation = _middleMan;
 	}
 }
