4d14d3bb8929f0b3a55432c3f6ef2a2f6995297a
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-06 22:00:32 -0400

    Fix fly double collect glitch

--- scripts/in_level_objects/flies/FlyBehavior.cs
+++ scripts/in_level_objects/flies/FlyBehavior.cs
@@ -3,19 +3,23 @@ using Godot;
 public partial class FlyBehavior : Area2D
 {
 	[Export] public Sprite2D Sprite;
+	private bool _noDoubleDown;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// Match level count to fly assets used
 		FlyCount.TotalLevelFlies++;
+
+		_noDoubleDown = true;
 	}
 
 	// Letting the player eat the fly
 	private void Collect(Node2D node)
 	{
-		if (node is Player || node is TongueProjectile || node.GetParent() is TongueLine)
+		if ((node is Player || node is TongueProjectile || node.GetParent() is TongueLine) && _noDoubleDown)
 		{
+			_noDoubleDown = false;
 			SoundManager.PlaySound(SFX.FlyCatch, GetTree().Root);
 			FlyCount.UpCount();
 			QueueFree();
