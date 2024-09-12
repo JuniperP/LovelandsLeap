5054f48dfdfbcdcd1fcac54d57850a16d4ddb843
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-12 15:48:34 -0400

    Fix FadeTransition while making it concise

--- scripts/in_level_objects/FadeTransition.cs
+++ scripts/in_level_objects/FadeTransition.cs
@@ -21,14 +21,11 @@ public partial class FadeTransition : Area2D
 			FlyCount.FliesGottenLevel = 0;
 			FlyCount.TotalLevelFlies = 0;
 
-			// Saves the game
-			LoadLevelData.SaveData(_sendsTo);
-
-
-
 			// Playing the leaving sfx
 			_leavingSFX.Play();
 
+
+			// Set up for after this level
 			if (_toEndCutscene)
 			{
 				// The run has finished!
@@ -36,29 +33,15 @@ public partial class FadeTransition : Area2D
 
 				// Giving the user the right ending
 				if (FlyCount.FliesGottenTotal == 0)
-				{
-					SceneManager.SetNextGoTo(ToScene.HungryEnding);
-					GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(ToScene.HungryEnding);
-				}
+					SetupNextArea(ToScene.HungryEnding);
 				else if (FlyCount.FliesGottenTotal == FlyCount.TotalGameFlies)
-				{
-					SceneManager.SetNextGoTo(ToScene.FullEnding);
-					GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(ToScene.FullEnding);
-				}
+					SetupNextArea(ToScene.FullEnding);
 				else
-				{
-					SceneManager.SetNextGoTo(ToScene.NormalEnding);
-					GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(ToScene.NormalEnding);
-				}
-
+					SetupNextArea(ToScene.NormalEnding);
 			}
 
-			// Sets up to go to the next area
 			else
-			{
-				SceneManager.SetNextGoTo(_sendsTo);
-				GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(_sendsTo);
-			}
+				SetupNextArea(_sendsTo);
 
 
 			// Starts into next scene
@@ -66,5 +49,18 @@ public partial class FadeTransition : Area2D
 
 
 		}
+	}
+
+
+	private static void SetupNextArea(ToScene newArea)
+	{
+		// Saves the game for the level to go to
+		LoadLevelData.SaveData(newArea);
+
+		// Sets the area to go to loading wise 
+		SceneManager.SetNextGoTo(newArea);
+
+		// Configures the new music
+		GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(newArea);
 	}
 }
