9cbb8d3cbfd2e51c12285a58a1411d1cfa28eb13
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-09-12 15:20:49 -0400

    Adjusting flies for the playtest level

--- scripts/ui/settings/key_binds/PlayTestButton.cs
+++ scripts/ui/settings/key_binds/PlayTestButton.cs
@@ -4,6 +4,13 @@ public partial class PlayTestButton : Button
 	// Telling the node that its time to play test
 	private static void PlayTest()
 	{
+		// Adjusting flies
+		FlyCount.FliesGottenLevel = 0;
+		FlyCount.TotalLevelFlies = 0;
+		LoadLevelData.LoadData();
+		FlyCount.FliesGottenLevelTotal = FlyCount.FliesGottenTotal;
+
+
 		GlobalMusicPlayer.ToPlay = GlobalMusicPlayer.GetSceneMusicID(ToScene.PlayTestLevel);
 		SceneManager.SetNextGoTo(ToScene.PlayTestLevel);
 		LoadingScreen.FadeIn();
